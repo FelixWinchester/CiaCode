869da74fa782dab23c1d13f6f60199de995ff207
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-09-07 21:31:42 +0400

    Create 1.cs

---
+++ 1.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первое число:");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(a + "+" + b + "+" + c + "=" + (a + b + c));
+
+    }
+}
