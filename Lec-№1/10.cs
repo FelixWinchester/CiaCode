2fc6332d1b320a10858a134a0e45891fbef956f4
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-09-08 12:27:16 +0400

    Update 10.cs

--- Lec-№1/10.cs
+++ Lec-№1/10.cs
@@ -3,11 +3,12 @@ class Task
 {
     static void Main()
     {
-        Console.WriteLine("Введите ваше имя:");
+        Console.Write("Введите ваше имя: ");
         string? name = Console.ReadLine();
-        Console.WriteLine("Введите ваш возраст:");
+        Console.Write("Введите ваш возраст: ");
         int age = Convert.ToInt32(Console.ReadLine());
         int rez = (2009 - age);
-        Console.WriteLine($"{name}, ты родился в {rez} году");
+        Console.Write($"{name}, ты родился в {rez} году");
+        
     }
 }
