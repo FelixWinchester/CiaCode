cf48c597eb3d0b3f8e91e4283a04b0c11c2970f7
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-09-10 23:35:36 +0400

    Create 8.cs

---
+++ Practicum_№3/8.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Task
+{
+    static void Main()
+    {
+        Console.Write("Введите первое число: ");
+        int A = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = A; i < B; i++)
+        {
+            if (i < 0) {
+
+            Console.Write($"{i} ");
+
+            }
+
+        }  
+    }
+}
