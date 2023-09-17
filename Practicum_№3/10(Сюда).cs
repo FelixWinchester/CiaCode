97784a288196740060ecfcd755a24ecf04e5888d
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-09-17 18:30:17 +0400

    Update 10(Сюда).cs

--- Practicum_№3/10(Сюда).cs
+++ Practicum_№3/10(Сюда).cs
@@ -11,7 +11,7 @@ class Task
 
         for (int i = A; i < B; i++)
         {
-            if (i % 2 == 0 && i % 3 == 0)
+            if (i % 6 == 0)
             {
                 Console.Write($"{i} ");
             }
