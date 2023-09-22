9306cfbb68d9128f827490a21ed72e67d1437ed3
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-09-22 11:28:35 +0400

    Update IV-15.cs

--- Practicum_№5/IV-15.cs
+++ Practicum_№5/IV-15.cs
@@ -1,1 +1,28 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите необходимое число: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Все возможные разложения числа {n} на слагаемые:");
+        PrintPartitions(n, n, "");
+    }
+
+    static void PrintPartitions(int n, int max, string partition)
+    {
+        if (n == 0)
+        {
+            Console.WriteLine(partition);
+            return;
+        }
+
+        for (int i = Math.Min(max, n); i >= 1; i--)
+        {
+            PrintPartitions(n - i, i, partition + i.ToString() + " ");
+        }
+    }
+}
 
