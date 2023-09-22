0f760f8321d15903c70ea9fab939e0460c6b269e
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-09-22 13:04:50 +0400

    Update 10(Сюда).cs

--- Practicum_№3/10(Сюда).cs
+++ Practicum_№3/10(Сюда).cs
@@ -8,16 +8,18 @@ class Task
         int A = Convert.ToInt32(Console.ReadLine());
         Console.Write("Введите второе число: ");
         int B = Convert.ToInt32(Console.ReadLine());
+        // Проверка числа на четность {Если оно нечетное, прибавляем к нему 1, делая его четным} //
         if (A % 2 == 1)
         {
             A += 1;
         }
-            for (int i = A; i < B; i += 2)
+        // Проходим по диапазону чисел и выводим нужные //
+        for (int i = A; i < B; i += 2)
+        {
+            if (i % 3 == 0)
             {
-                if (i % 3 == 0)
-                {
-                    Console.Write($"{i} ");
-                }
+                Console.Write($"{i} ");
             }
         }
     }
+}
