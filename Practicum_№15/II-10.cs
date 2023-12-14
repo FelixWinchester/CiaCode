847b3c19f596b2b17b99d7dec2275c14504dfafb
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-12-14 14:35:33 +0400

    Update II-10.cs

--- Practicum_№15/II-10.cs
+++ Practicum_№15/II-10.cs
@@ -2,59 +2,59 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 
-struct Employee
+struct Employee // создание структуры Employee
 {
-    public string FullName;
+    public string FullName; // определение полей структуры Employee
     public int HireYear;
     public string Position;
     public decimal Salary;
     public int WorkExperience;
 }
 
-class Program
+class Program // создание класса Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args) // определение точки входа в программу
     {
-        List<Employee> employees = new List<Employee>();
+        List<Employee> employees = new List<Employee>(); // создание списка сотрудников
 
         // Чтение данных из файла input.txt
-        string[] lines = File.ReadAllLines("input.txt");
-        foreach (string line in lines)
+        string[] lines = File.ReadAllLines("input.txt"); // чтение всех строк из файла input.txt
+        foreach (string line in lines) // итерация по каждой строке
         {
-            string[] parts = line.Split(',');
-            Employee employee = new Employee
+            string[] parts = line.Split(','); // разделение строки на части
+            Employee employee = new Employee // создание нового сотрудника
             {
-                FullName = parts[0],
+                FullName = parts[0], // присвоение значений полям сотрудника
                 HireYear = int.Parse(parts[1]),
                 Position = parts[2],
                 Salary = decimal.Parse(parts[3]),
                 WorkExperience = int.Parse(parts[4])
             };
-            employees.Add(employee);
+            employees.Add(employee); // добавление сотрудника в список
         }
 
         // Группировка сотрудников по должности
-        Dictionary<string, List<Employee>> groupedEmployees = new Dictionary<string, List<Employee>>();
-        foreach (Employee employee in employees)
+        Dictionary<string, List<Employee>> groupedEmployees = new Dictionary<string, List<Employee>>(); // создание словаря для группировки сотрудников
+        foreach (Employee employee in employees) // итерация по каждому сотруднику
         {
-            if (!groupedEmployees.ContainsKey(employee.Position))
+            if (!groupedEmployees.ContainsKey(employee.Position)) // проверка наличия ключа в словаре
             {
-                groupedEmployees[employee.Position] = new List<Employee>();
+                groupedEmployees[employee.Position] = new List<Employee>(); // добавление нового ключа в словарь
             }
-            groupedEmployees[employee.Position].Add(employee);
+            groupedEmployees[employee.Position].Add(employee); // добавление сотрудника в соответствующую группу
         }
 
         // Запись результирующей информации в файл output.txt
-        using (StreamWriter writer = new StreamWriter("output.txt"))
+        using (StreamWriter writer = new StreamWriter("output.txt")) // открытие файла для записи
         {
-            foreach (KeyValuePair<string, List<Employee>> group in groupedEmployees)
+            foreach (KeyValuePair<string, List<Employee>> group in groupedEmployees) // итерация по каждой группе
             {
-                writer.WriteLine($"Должность: {group.Key}");
-                foreach (Employee employee in group.Value)
+                writer.WriteLine($"Должность: {group.Key}"); // запись названия должности
+                foreach (Employee employee in group.Value) // итерация по сотрудникам в группе
                 {
-                    writer.WriteLine($"{employee.FullName}, {employee.HireYear}, {employee.Position}, {employee.Salary}, {employee.WorkExperience}");
+                    writer.WriteLine($"{employee.FullName}, {employee.HireYear}, {employee.Position}, {employee.Salary}, {employee.WorkExperience}"); // запись информации о сотруднике
                 }
-                writer.WriteLine();
+                writer.WriteLine(); // запись пустой строки для разделения групп
             }
         }
     }
