f1fd92009021010054ea1e685adaf100f24e65dd
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-12-14 23:39:25 -0800

    Update I-10.cs

--- Practicum_№14/I-10.cs
+++ Practicum_№14/I-10.cs
@@ -1,89 +1,60 @@
-using System;
-using System.IO;
-using System.Collections.Generic;
+using System; // подключение пространства имен System
+using System.IO; // подключение пространства имен System.IO
+using System.Collections.Generic; // подключение пространства имен System.Collections.Generic
 
-struct SPoint
+struct SPoint // объявление структуры SPoint
 {
-    public double X { get; set; }
-    public double Y { get; set; }
+    public double X { get; set; } // объявление свойства X типа double
+    public double Y { get; set; } // объявление свойства Y типа double
 }
 
-class Program
+class Program // объявление класса Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args) // объявление метода Main с массивом аргументов args
     {
         // Чтение данных из файла
-        string[] lines = File.ReadAllLines("points.txt");
-        List<SPoint> points = new List<SPoint>();
-        foreach (string line in lines)
+        string[] lines = File.ReadAllLines("points.txt"); // чтение строк из файла "points.txt"
+        List<SPoint> points = new List<SPoint>(); // создание списка точек
+        foreach (string line in lines) // цикл по строкам файла
         {
-            string[] parts = line.Split(' ');
-            double x = double.Parse(parts[0]);
-            double y = double.Parse(parts[1]);
-            points.Add(new SPoint { X = x, Y = y });
+            string[] parts = line.Split(' '); // разделение строки на части по пробелу
+            double x = double.Parse(parts[0]); // парсинг первой части как X
+            double y = double.Parse(parts[1]); // парсинг второй части как Y
+            points.Add(new SPoint { X = x, Y = y }); // добавление новой точки в список
         }
 
         // Поиск точки, для которой шар максимального радиуса содержит максимальное число точек
-        double maxRadius = 0;
-        SPoint center = new SPoint();
-        foreach (SPoint point in points)
+        double maxRadius = 0; // инициализация переменной maxRadius
+        SPoint center = new SPoint(); // инициализация переменной center
+        foreach (SPoint point in points) // цикл по точкам
         {
-            double radius = 0;
-            int count = 0;
-            foreach (SPoint otherPoint in points)
+            double radius = 0; // инициализация переменной radius
+            int count = 0; // инициализация переменной count
+            foreach (SPoint otherPoint in points) // вложенный цикл по другим точкам
             {
-                double distance = Math.Sqrt(Math.Pow(point.X - otherPoint.X, 2) + Math.Pow(point.Y - otherPoint.Y, 2));
-                if (distance <= radius)
+                double distance = Math.Sqrt(Math.Pow(point.X - otherPoint.X, 2) + Math.Pow(point.Y - otherPoint.Y, 2)); // вычисление расстояния между точками
+                if (distance <= radius) // проверка условия
                 {
-                    count++;
+                    count++; // увеличение счетчика
                 }
                 else
                 {
-                    break;
+                    break; // выход из цикла
                 }
-                radius++;
+                radius++; // увеличение радиуса
             }
-            if (count > maxRadius)
+            if (count > maxRadius) // проверка условия
             {
-                maxRadius = count;
-                center = point;
+                maxRadius = count; // присваивание нового значения maxRadius
+                center = point; // присваивание нового значения center
             }
         }
 
         // Запись результата в файл
-        using (StreamWriter writer = new StreamWriter("result.txt"))
+        using (StreamWriter writer = new StreamWriter("result.txt")) // использование StreamWriter для записи в файл "result.txt"
         {
-            writer.WriteLine("Координаты центра: " + center.X + " " + center.Y);
-            writer.WriteLine("Максимальное число точек: " + maxRadius);
+            writer.WriteLine("Координаты центра: " + center.X + " " + center.Y); // запись координат центра в файл
+            writer.WriteLine("Максимальное число точек: " + maxRadius); // запись максимального числа точек в файл
         }
     }
 }
-/* points.txt
-1 2
-3 4
-5 6
-7 8
-9 10
-11 12
-13 14
-15 16
-17 18
-19 20
-21 22
-23 24
-25 26
-27 28
-29 30
-31 32
-33 34
-35 36
-37 38
-39 40
-41 42
-43 44
-45 46
-47 48
-49 50
-*/
-// result.txt : Координаты центра: 1 2
-   //           Максимальное число точек: 1
