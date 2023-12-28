fcfff70a3dca8cda61df7d966236f80ff3a45bfa
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-12-29 00:44:59 +0400

    Update II-10.cs

--- Practicum_№14/II-10.cs
+++ Practicum_№14/II-10.cs
@@ -1,69 +1,105 @@
 using System;
 using System.IO;
-using System.Linq;
 
-public class Student : IComparable<Student>
+class chisla
 {
-    public string FullName { get; set; }
-    public int Course { get; set; }
-    public string Group { get; set; }
-    public int RunResult { get; set; }
-
-    public int CompareTo(Student other)
+    public struct Sgroup : IComparable<Sgroup>
     {
-        return other.RunResult.CompareTo(RunResult);
-    }
-}
+        public string fam, name, second_name;
+        public int group, course;
+        public double time;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        var students = new Student[100];
-        int count = 0;
-
-        using (var reader = new StreamReader("input.txt"))
+        public Sgroup(string fam, string name, string second_name, int group, int course, double time)
         {
-            string line;
-            while ((line = reader.ReadLine()) != null)
+            this.name = name;
+            this.fam = fam;
+            this.second_name = second_name;
+            this.group = group;
+            this.course = course;
+            this.time = time;
+        }
+
+        public int CompareTo(Sgroup obj)
+        {
+            if (this.time > obj.time) return -1;
+            else if (this.time < obj.time) return 1;
+            else return 0;
+        }
+
+        public void Show(StreamWriter fileout)
+        {
+            fileout.Write("{0} {1} {2} {3} {4} {5}", name, fam, second_name, group, course, time);
+            fileout.WriteLine();
+        }
+
+        public void Show()
+        {
+            Console.Write("{0} {1} {2} {3} {4} {5}", name, fam, second_name, group, course, time);
+            Console.WriteLine();
+        }
+    }
+
+    static public Sgroup[] Input(ref int n)
+    {
+        using (StreamReader fileIn = new StreamReader("input.txt"))
+        {
+            n = int.Parse(fileIn.ReadLine());
+            Sgroup[] ar = new Sgroup[n];
+            for (int i = 0; i < n; i++)
             {
-                var parts = line.Split(';');
-                students[count] = new Student
+                string[] text = fileIn.ReadLine().Split(' ');
+                ar[i] = new Sgroup(text[0], text[1], text[2], int.Parse(text[3]), int.Parse(text[4]), double.Parse(text[5]));
+            }
+            return ar;
+        }
+    }
+
+    static void Print(Sgroup[] array)
+    {
+        foreach (Sgroup student in array)
+        {
+            student.Show();
+        }
+    }
+
+    static void Main()
+    {
+        int n = 0;
+        Sgroup[] groupmates = Input(ref n);
+        Array.Sort(groupmates);
+        int count = 1;
+        using (StreamWriter fileout = new StreamWriter("output.txt"))
+        {
+            fileout.WriteLine("Top 3 Students:");
+            groupmates[0].Show(fileout);
+
+            for (int i = 1; i < n; i++)
+            {
+                if (count < 3 || groupmates[i].time == groupmates[i - 1].time)
                 {
-                    FullName = parts[0],
-                    Course = int.Parse(parts[1]),
-                    Group = parts[2],
-                    RunResult = int.Parse(parts[3])
-                };
-                count++;
+                    groupmates[i].Show(fileout);
+
+                    if (groupmates[i].time != groupmates[i - 1].time)
+                    {
+                        count++;
+                    }
+                    else if (count == 3)
+                    {
+                        count++;
+                        fileout.WriteLine("Additional Students with the Same Result:");
+                    }
+                }
             }
         }
 
-        Array.Sort(students, 0, count);
-
-        using (var writer = new StreamWriter("output.txt"))
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                writer.WriteLine(students[i].FullName + " " + students[i].Course + " " + students[i].Group + " " + students[i].RunResult);
-            }
-        }
+        chisla.Print(groupmates);
     }
 }
-/* input.txt
-Иванов;1;1;10
-Петров;2;2;15
-Сидоров;3;3;20
-Кузнецов;1;1;10
-Смирнов;2;2;15
-Алексеев;3;3;20
-Васильев;1;1;10
-Николаев;2;2;15
-Александров;3;3;20
-Игнатьев;1;1;10
-*/
-/* output.txt
-Сидоров 3 3 20
-Алексеев 3 3 20
-Александров 3 3 20
+/*
+5
+Иванов Иван Иванович 1 2 10.5
+Петров Петр Петрович 2 1 9.8
+Сидоров Сидор Сидорович 3 1 10.2
+Кузнецов Андрей Иванович 4 2 9.5
+Михайлова Елена Викторовна 5 1 10.2
 */
