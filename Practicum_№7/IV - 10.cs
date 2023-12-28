4d9ab133859daee224328f62b48029cfe521254e
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-12-28 20:41:57 +0400

    Update IV - 10.cs

--- Practicum_№7/IV - 10.cs
+++ Practicum_№7/IV - 10.cs
@@ -1,73 +1,84 @@
 using System;
+
 class Program
 {
     static void Main()
     {
-        int n = GetArraySize();
-        int[,] array = CreateAndFillArray(n);
-        int[] resultArray = FindPositiveElements(array, n);
-        PrintResults(resultArray, array, n);
+        Console.Write("Введите размерность массива n: ");
+        if (int.TryParse(Console.ReadLine(), out int n) && n > 0)
+        {
+            int[,] originalArray = GenerateRandomArray(n);
+            Console.WriteLine("Исходный массив:");
+            PrintArray(originalArray);
+
+            int[] firstPositiveElements = FindFirstPositiveElements(originalArray);
+            Console.WriteLine("\nПервые положительные элементы каждого столбца:");
+            PrintArray(firstPositiveElements);
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Введите натуральное число больше 0.");
+        }
     }
 
-    static int GetArraySize()
-    {
-        Console.Write("Введите размер массива n: ");
-        int n = Convert.ToInt32(Console.ReadLine());
-        return n;
-    }
-
-    static int[,] CreateAndFillArray(int n)
+    static int[,] GenerateRandomArray(int n)
     {
         int[,] array = new int[n, n];
-        Console.WriteLine("Введите элементы массива:");
+        Random random = new Random();
+
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
             {
-                Console.Write($"Массив[{i},{j}]: ");
-                array[i, j] = Convert.ToInt32(Console.ReadLine());
+                array[i, j] = random.Next(-10, 11); // Заполняем случайными числами от -10 до 10
             }
         }
+
         return array;
     }
-    
-    static int[] FindPositiveElements(int[,] array, int n)
+
+    static void PrintArray(int[,] array)
     {
-        int[] resultArray = new int[n];
-        for (int j = 0; j < n; j++)
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
         {
-            int positiveElement = 0;
-            for (int i = 0; i < n; i++)
+            for (int j = 0; j < cols; j++)
             {
-                if (array[i, j] > 0)
-                {
-                    positiveElement = array[i, j];
-                    break;
-                }
-            }
-            resultArray[j] = positiveElement;
-        }
-        return resultArray;
-    }
-
-
-    static void PrintResults(int[] resultArray, int[,] array, int n)
-    {
-        Console.WriteLine("Результаты:");
-        Console.WriteLine("Массив с результатами:");
-        for (int j = 0; j < n; j++)
-        {
-            Console.WriteLine($"Результат Массива[{j}]: {resultArray[j]}");
-        }
-
-        Console.WriteLine("Конечный массив:");
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                Console.Write($"{array[i, j]} ");
+                Console.Write(array[i, j] + "\t");
             }
             Console.WriteLine();
         }
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (int element in array)
+        {
+            Console.Write(element + "\t");
+        }
+        Console.WriteLine();
+    }
+
+    static int[] FindFirstPositiveElements(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+        int[] firstPositiveElements = new int[cols];
+
+        for (int j = 0; j < cols; j++)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                if (array[i, j] >= 0) // Условие изменено, теперь 0 считается положительным
+                {
+                    firstPositiveElements[j] = array[i, j];
+                    break; // Найден положительный элемент, переходим к следующему столбцу
+                }
+            }
+        }
+
+        return firstPositiveElements;
     }
 }
