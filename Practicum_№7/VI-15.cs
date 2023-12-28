027bb9365e2810c864a989ff3f92823dc04849e2
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-12-28 23:14:00 +0400

    Update VI-15.cs

--- Practicum_№7/VI-15.cs
+++ Practicum_№7/VI-15.cs
@@ -4,70 +4,129 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Введите размер массива n: ");
+        Console.Write("Введите размерность массива n: ");
         int n = int.Parse(Console.ReadLine());
-        int[,] arr = new int[n, n];
-        Console.WriteLine("Введите элементы массива:");
+
+        // Инициализация ступенчатого массива
+        int[][] array = new int[n][];
         for (int i = 0; i < n; i++)
         {
+            Console.WriteLine($"Введите элементы {i + 1}-й строки через пробел:");
+            string[] input = Console.ReadLine().Split(' ');
+            array[i] = new int[n];
             for (int j = 0; j < n; j++)
             {
-                arr[i, j] = int.Parse(Console.ReadLine());
+                array[i][j] = int.Parse(input[j]);
             }
         }
 
-        int[,] newArr = new int[n, n];
-        int newN = n;
-        for (int i = 0; i < n; i++)
+        Console.WriteLine("\nИсходный массив:");
+        PrintArray(array);
+
+        // Уплотнение массива
+        int[][] resultArray = CompactArray(array);
+
+        Console.WriteLine("\nРезультат уплотнения:");
+        PrintArray(resultArray);
+    }
+
+    static int[][] CompactArray(int[][] inputArray)
+    {
+        int rowCount = inputArray.Length;
+        int colCount = inputArray[0].Length;
+
+        // Флаги для определения, какие строки и столбцы нужно удалить
+        bool[] deleteRows = new bool[rowCount];
+        bool[] deleteCols = new bool[colCount];
+
+        // Проверка каждой строки на наличие ненулевых элементов
+        for (int i = 0; i < rowCount; i++)
         {
-            bool isRowZero = true;
-            for (int j = 0; j < n; j++)
+            for (int j = 0; j < colCount; j++)
             {
-                if (arr[i, j] != 0)
+                if (inputArray[i][j] != 0)
                 {
-                    isRowZero = false;
+                    // Если найден ненулевой элемент, то строку не удаляем
+                    deleteRows[i] = false;
                     break;
                 }
-            }
-            if (!isRowZero)
-            {
-                for (int j = 0; j < n; j++)
+                else
                 {
-                    newArr[i, j] = arr[i, j];
+                    // Если строка состоит из одних нулей, то помечаем ее для удаления
+                    deleteRows[i] = true;
                 }
-            }
-            else
-            {
-                newN--;
             }
         }
 
-        for (int i = 0; i < n; i++)
+        // Проверка каждого столбца на наличие ненулевых элементов
+        for (int j = 0; j < colCount; j++)
         {
-            bool isColZero = true;
-            for (int j = 0; j < n; j++)
+            for (int i = 0; i < rowCount; i++)
             {
-                if (arr[j, i] != 0)
+                if (inputArray[i][j] != 0)
                 {
-                    isColZero = false;
+                    // Если найден ненулевой элемент, то столбец не удаляем
+                    deleteCols[j] = false;
                     break;
                 }
-            }
-            if (!isColZero)
-            {
-                for (int j = 0; j < newN; j++)
+                else
                 {
-                    newArr[j, i] = arr[j, i];
+                    // Если столбец состоит из одних нулей, то помечаем его для удаления
+                    deleteCols[j] = true;
                 }
             }
         }
 
-        Console.WriteLine("Уплотненный массив:");
-        for (int i = 0; i < newN; i++)
+        // Создаем новый массив с учетом флагов удаления строк и столбцов
+        int newRowCount = rowCount - CountTrueValues(deleteRows);
+        int newColCount = colCount - CountTrueValues(deleteCols);
+
+        int[][] resultArray = new int[newRowCount][];
+        int rowIndex = 0;
+
+        for (int i = 0; i < rowCount; i++)
         {
-            for (int j = 0; j < newN; j++)
+            if (!deleteRows[i])
             {
-                Console.Write(newArr[i, j] + " ");
+                resultArray[rowIndex] = new int[newColCount];
+                int colIndex = 0;
+
+                for (int j = 0; j < colCount; j++)
+                {
+                    if (!deleteCols[j])
+                    {
+                        resultArray[rowIndex][colIndex] = inputArray[i][j];
+                        colIndex++;
+                    }
+                }
+
+                rowIndex++;
+            }
+        }
+
+        return resultArray;
+    }
+
+    static int CountTrueValues(bool[] array)
+    {
+        int count = 0;
+        foreach (bool value in array)
+        {
+            if (value)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static void PrintArray(int[][] array)
+    {
+        foreach (var row in array)
+        {
+            foreach (var element in row)
+            {
+                Console.Write($"{element} ");
             }
             Console.WriteLine();
         }
